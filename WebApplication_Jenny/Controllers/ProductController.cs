154a79f8f95df5385616fe5ad24816439d48d421
MTHuang <siksj3@gmail.com>
2022-01-02 14:59:37 +0800

    Create ProductController、Enable CORS

---
+++ WebApplication_Jenny/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication_Jenny.Controllers
+{
+    [Route("[controller]")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ProductController : ApiController
+    {
+
+    }
+}
